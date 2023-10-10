017800aad1ac75b12e19fec4bdd7e2ceb74c88bb
GidyMane <muragegideon2000@gmail.com>
2023-10-10 18:45:47 +0300

    Send Data TO SERVER

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    const configurationObject={
+        method:"POST", 
+        headers:{
+            "Content-Type":"application/json",
+            "accept": "application/json"
+        },
+        body:JSON.stringify({
+            "name":name,
+            "email":email
+        })
+    }
+
+    fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(function(object){
+        // console.log(object)
+        
+        let p = document.createElement('p')
+        p.innerHTML=object.id
+        let body =document.querySelector("body")
+        body.appendChild(p)
+    }).catch(e=>console.log(e))
+}
+
